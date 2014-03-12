715cc50eb65926357bd1ab30d1d92805e0b1b7c6
chie voeltz <chonda39@gmail.com>
2014-03-11 19:17:47 -0500

    exercises 6-1-2-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ActiveRecord::Base
 	#a block to the before_save callback and sets the user’s email 
 	#address to a lower-case version of its current value using 
 	#the downcase string method.
-  before_save { self.email = email.downcase }		
+  #before_save { self.email = email.downcase }	--this is same things with below
+  before_save { email.downcase! }	
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },	
   			        uniqueness:{ case_sensitive: false }
 
