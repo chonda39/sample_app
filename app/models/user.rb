126cf96ea7d9931e12e9c4ea457549459235241b
chie voeltz <chonda39@gmail.com>
2014-04-02 20:55:30 -0500

    Initial Chapter 8 run through

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,27 @@ class User < ActiveRecord::Base
 	#the downcase string method.
   #before_save { self.email = email.downcase }	--this is same things with below
   before_save { email.downcase! }	
+  before_create :create_remember_token
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },	
   			        uniqueness:{ case_sensitive: false }
 
   has_secure_password
-  validates :password, length: { minimum: 6 }			        
+  validates :password, length: { minimum: 6 }	
+
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.hash(User.new_remember_token)
+    end	        
 end
